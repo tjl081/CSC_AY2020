19446f9294ff831abacda82f0bf997b0422f55ac
lesterlimjj <54622759+lesterlimjj@users.noreply.github.com>
2020-12-25 15:49:22 +0800

    Task 2 done

---
+++ task 2/WebAPI2/Controllers/ProductsV1Controller.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    public class ProductsV1Controller : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        [HttpGet]
+        [Route("api/v1/products")]
+        //https://localhost:44384/api/v1/products
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+
+        }
+
+        [HttpGet]
+        [Route("api/v1/products")]
+        //https://localhost:44384/api/v1/products
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [HttpGet]
+        [Route("api/v1/products/{id:int:min(1)}", Name = "getProductById")]
+        //https://localhost:44384/api/v1/products/1
+        public HttpResponseMessage GetProduct(int id)
+        {
+            var product = repository.Get(id);
+            if (product == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Unable to find product!");
+            }
+            return Request.CreateResponse<Product>(HttpStatusCode.OK, product);
+        }
+
+        [HttpPost]
+        [Route("api/v1/products")]
+        //https://localhost:44384/api/v1/products
+        public HttpResponseMessage CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product = repository.Add(product);
+                var response = Request.CreateResponse<Product>(HttpStatusCode.Created, product);
+
+                string uri = Url.Link("getProductById", new { id = product.Id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+            
+
+        }
+
+        [HttpPut]
+        [Route("api/v1/products/{id:int:min(1)}")]
+        //https://localhost:44384/api/v1/products/2
+        public HttpResponseMessage UpdateProduct(Product product, int id)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Product updating failed!");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Product updated successfully!");
+        }
+
+        [HttpDelete]
+        [Route("api/v1/products/{id:int:min(1)}")]
+        //https://localhost:44384/api/v1/products/1
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Product deleting failed!");
+            }
+
+            repository.Remove(id);
+            return Request.CreateResponse(HttpStatusCode.OK, "Product deleted successfully!");
+
+        }
+    }
+}
---
+++ task 2/WebAPI2/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Invalid price")]
+        public decimal Price { get; set; }
+    }
+}
