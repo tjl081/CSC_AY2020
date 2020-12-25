81e65fb5bd1a5256386f8fe7a2a389ed6e14c638
jialiang <jialiang.18@ichat.sp.edu.sg>
2020-12-25 21:13:31 +0800

    task 3 code done

---
+++ CSC_AY2020_Task3/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CSC_AY2020_Task3.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+
+        // GET api/values
+        public string Get()
+        {
+            var userName = this.RequestContext.Principal.Identity.Name;
+            return String.Format("Hello, {0}.", userName);
+        }
+
+    }
+}
