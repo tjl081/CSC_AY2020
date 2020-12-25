62af1250ef00f3e6fd38e0fcc29b6a142064472a
jialiang <jialiang.18@ichat.sp.edu.sg>
2020-12-26 01:32:44 +0800

    secret key ommited

--- CSC_AY2020_Task3/Models/CaptchaRequest.cs
+++ CSC_AY2020_Task3/Models/CaptchaRequest.cs
@@ -9,7 +9,7 @@ namespace CSC_AY2020_Task3.Models
     {
 
         
-        public string secret { get; set; } = "6LdsMwYaAAAAAK0BYrBD0mSxu9cYl9nF1JsRrT1Z"; //you're supposed to have a version yourself
+        public string secret { get; set; } = ""; //you're supposed to have a version yourself
         public string response { get; set; }
     }
 }
