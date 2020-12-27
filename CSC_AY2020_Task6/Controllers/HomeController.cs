4971e89ce3951682dc7c8b2e3d444885c8cf214f
jialiang <jialiang.18@ichat.sp.edu.sg>
2020-12-28 02:19:58 +0800

    customer creation works, validation not complete

--- CSC_AY2020_Task6/Controllers/HomeController.cs
+++ CSC_AY2020_Task6/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CSC_AY2020_Task6.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Stripe;
 
 namespace CSC_AY2020_Task6.Controllers
 {
@@ -13,6 +16,62 @@ namespace CSC_AY2020_Task6.Controllers
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public ActionResult Charge()
+        {
+            ViewBag.Message = "Learn how to process payments with Stripe";
+            return View(new RegistrationModel());
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Charge(RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var chargeId = await RegisterCustomer(model);
+            // You should do something with the chargeId --> Persist it maybe?
+
+            return View("PlanSelect");
+        }
+
+
+
+        private static async Task<Stripe.Customer> RegisterCustomer(RegistrationModel model)
+        {
+            return await Task.Run(() =>
+            {
+                StripeConfiguration.ApiKey = "sk_test_51I2bbHBYTNT0yLeeR90ErQBVJPbr5wYhg9NySyKsKb3HpFJ64fiTvVAsSYQpq53Vx4yk180e4uJbLMZ1F7AwhUTD00bbyfNUiO";
+
+
+                var custOptions = new CustomerCreateOptions
+                {
+                    
+                    Name = model.Name,
+                    Phone = model.Phone,
+                    Email = model.Email,
+                    Address = new AddressOptions()
+                    {
+                        Line1 = model.AddressLine1,
+                        Line2 = model.AddressLine2,
+                        PostalCode = model.AddressPostcode,
+                        City = model.AddressCity,
+                        Country = model.AddressCountry
+                    }
+
+                };
+
+                var service = new CustomerService();
+                var account = service.Create(custOptions);
+
+                return account;
+            });
         }
     }
 }
---
+++ CSC_AY2020_Task6/Controllers/StripeController.cs
@@ -0,0 +1,51 @@
+﻿using CSC_AY2020_Task6.Models;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace CSC_AY2020_Task6.Controllers
+{
+    public class StripeController : ApiController
+    {
+
+
+        [HttpPost]
+        [Route("Register")]
+        private static async Task<Stripe.Customer> RegisterCustomer(RegistrationModel model)
+        {
+            return await Task.Run(() =>
+            {
+                StripeConfiguration.ApiKey = "sk_test_51I2bbHBYTNT0yLeeR90ErQBVJPbr5wYhg9NySyKsKb3HpFJ64fiTvVAsSYQpq53Vx4yk180e4uJbLMZ1F7AwhUTD00bbyfNUiO";
+
+
+                var custOptions = new CustomerCreateOptions
+                {
+
+                    Name = model.Name,
+                    Phone = model.Phone,
+                    Email = model.Email,
+                    Address =
+                    {
+                        Line1 = model.AddressLine1,
+                        Line2 = model.AddressLine2,
+                        PostalCode = model.AddressPostcode,
+                        City = model.AddressCity,
+                        Country = model.AddressCountry
+                    }
+
+                };
+
+                var service = new CustomerService();
+                var account = service.Create(custOptions);
+
+                return account;
+            });
+        }
+    }
+}
